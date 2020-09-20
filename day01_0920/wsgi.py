c9cf939805eb23d48afb5c58389da3e6344e12f7
unknown <luojun0115@126.com>
2020-09-20 22:30:47 +0800

    Initial commit

---
+++ day01_0920/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for day01_0920 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'day01_0920.settings')
+
+application = get_wsgi_application()
